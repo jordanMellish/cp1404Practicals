300d74d7d04e54276acea48e9bb82899ac25bc8b
jordanMellish <jordanjmellish@gmail.com>
2018-08-20 21:30:57 +1000

    refactor password checker to use functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -6,12 +6,21 @@ MIN_LENGTH = 2
 
 
 def main():
-    password = input('Enter password')
+    password = get_password()
+    convert_password_to_asterisk(password)
+
+
+def convert_password_to_asterisk(password):
+    for char in password:
+        print('*', end="")
+
+
+def get_password():
+    password = input('Enter password: ')
     while not is_valid_password(password):
         print('Invalid password,try again')
         password = input('Enter password')
-    for char in password:
-        print('*', end="")
+    return password
 
 
 def is_valid_password(password):
