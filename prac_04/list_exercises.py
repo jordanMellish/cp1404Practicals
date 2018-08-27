3044a8e432fe114f784e494c664be3063501f3e0
jordanMellish <jordan.mellish@my.jcu.edu.au>
2018-08-28 09:56:44 +1000

    add remaining practical task to repo

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,25 @@
+"""
+List Exercises
+
+"""
+
+# # 1. Number sorter
+numbers = []
+for i in range(5):
+    number = int(input("Number :"))
+    numbers.append(number)
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[-1]))
+print("The smallest number is {}".format(min(numbers)))
+print("The largest number is {}".format(max(numbers)))
+print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
+#2. Woefully inadequate security checker.
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+             'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input('Enter Username : ')
+if username in usernames:
+    print('Access Granted')
+else:
+    print('Access Denied')
---
+++ prac_04/quick_picks.py
@@ -0,0 +1,27 @@
+"""
+Quick Picks program
+"""
+
+import random
+
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+def main():
+    number_of_quick_picks = int(input('How many quick picks? '))
+    while number_of_quick_picks < 0:
+        print("Can't have zero quick picks")
+        number_of_quick_picks = int(input('How many quick picks'))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for n in range(NUMBERS_PER_LINE):
+            number = random.randint(MINIMUM,MAXIMUM)
+            while number in quick_pick:
+                number = random.randint(MINIMUM,MAXIMUM)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join("{:2}". format(number) for number in quick_pick))
+
+main()
