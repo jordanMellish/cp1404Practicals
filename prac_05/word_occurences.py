9c738f62865164dfbd61066c51bb402130b869ba
jordanMellish <jordan.mellish@my.jcu.edu.au>
2018-09-04 10:12:03 +1000

    add word count program

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,19 @@
+"""
+Count word occurrences in a string
+"""
+
+unique_words = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    frequency = unique_words.get(word, 0)
+    unique_words[word] = frequency + 1
+
+
+# Sort each for word alphabetically
+words = list(unique_words.keys())
+words.sort()
+
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_length, unique_words[word]))
