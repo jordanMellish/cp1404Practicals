52de28d96646905b273435b8ff57731386c48dcc
jordanMellish <jordan.mellish@my.jcu.edu.au>
2018-09-04 10:01:45 +1000

    add for loop for state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,12 +6,17 @@ File needs reformatting
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" : "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
-# print(STATE_NAMES)
 
-state = input("Enter short state: ")
+keys = list(STATE_NAMES.keys())
+for key in keys:
+    print('{:3} is {}'.format(key, STATE_NAMES[key]))
+
+
+state = input("Enter short state: ").upper()
 while state != "":
     if state in STATE_NAMES:
         print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ")
+    state = input("Enter short state: ").upper()
+
