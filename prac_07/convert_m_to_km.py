898b5223fcf4359d69123902db1c7969e4199475
jordanMellish <jordan.mellish@my.jcu.edu.au>
2018-10-02 09:48:38 +1000

    add conversion program

---
+++ prac_07/convert_m_to_km.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical
+Kivy GUI program to convert Miles to Kilometers
+Jordan Mellish, IT@JCU
+Started 1/10/2018
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
--- prac_07/squaring.py
+++ prac_07/squaring.py
@@ -1,15 +1,15 @@
 """
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
+Jordan Mellish, IT@JCU
+Started 21/09/2018
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Jordan Mellish'
 
 
 class SquareNumberApp(App):
